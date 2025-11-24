41fcc2e36aec73f4de15efa5a06497d0af530ac9
Enes Söğüt <enessogut@outlook.com>
2025-11-24 14:03:52 +0300

    Routes Configured

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func Main() {
+import (
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/middleware"
+	"golang-restaurant-management/routes"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func Main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
 }
